e6afd341c3bca3cb63e3611a2844158298d701d0
R M A Sampath <amilasampath0130@gmail.com>
2026-05-02 13:40:20 +0530

    Feat:Update the userinterface

--- mobile_app/app/(tabs)/index.tsx
+++ mobile_app/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { Route, router } from "expo-router";
@@ -10,13 +10,31 @@ export default function HomeScreen() {
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
         <View style={styles.header}>
-          <Text style={styles.title}>Home</Text>
-        <Text style={styles.subtitle}>Long-Range Communication mesh for  Disaster Response and Relief Coordination</Text>
+          <Text style={styles.title}>Disaster response mesh network</Text>
+        <Text style={styles.subtitle}>Secure. Reliable. Connected.</Text>
+        <Text style={styles.subtitle}>Building a stronger response network when it matters most.</Text>
         </View>
-        <Button title="Go to Send Message" onPress={() => router.push("/(message)/sendMessage")} />
-        <Button title="Go to find location" onPress={() => router.push("/(message)/location")} />
-        <Button title="Go to node status" onPress={() => router.push("/(message)/nodeStatus")} />
-        <Button title="Go to settings" onPress={() => router.push("/(message)/setting")} />
+        <View style={styles.headerImageContainer}>
+          <Image
+            source={require('../../assets/images/disaster_mesh_mobile_banner_200px.png')}
+            style={styles.headerImage}
+            resizeMode="cover"
+          />
+
+        </View>
+        <TouchableOpacity style={styles.button} onPress={() => router.push("/(message)/sendMessage")}>
+          <Text style={styles.buttonText}>Send Message</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, { marginTop: 16 }]} onPress={() => router.push("/(message)/receivedMessage")}>
+          <Text style={styles.buttonText}>View messages</Text>
+        </TouchableOpacity> 
+        <TouchableOpacity style={[styles.button, { marginTop: 16 }]} onPress={() => router.push("/(message)/location")}>
+          <Text style={styles.buttonText}>View location</Text>
+        </TouchableOpacity> 
+        <TouchableOpacity style={[styles.button, { marginTop: 16 }]} onPress={() => router.push("/(message)/nodeStatus")}>
+          <Text style={styles.buttonText}>View node status</Text>
+        </TouchableOpacity> 
+
       </View>
     </SafeAreaView>
   );
@@ -36,20 +54,61 @@ const styles = StyleSheet.create({
     marginBottom: 24,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#ffffff",
+    padding: 16,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
   },
   title: {
-    fontSize: 24,
+    fontSize: 22,
     fontWeight: "700",
-    color: "#000",
+    color: "#313131",
     marginBottom: 8,
     justifyContent: "center",
     alignItems: "center",
     textAlign: "center",
 
+
   },
   subtitle: {
     fontSize: 16,
-    color: "#000",
+    color: "#888888",
+    textAlign: "center",
   },
-
+  headerImageContainer: {
+    marginBottom: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  headerImage: {
+    width: "100%",
+    height: 200,
+    // backgroundColor: "#ffffff",
+    marginBottom: 24,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    // shadowColor: "#000",
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowOpacity: 0.3,
+    // shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    backgroundColor: "#424242",
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#ffffff",
+    fontSize: 16,
+    fontWeight: "600",
+  },
 });
